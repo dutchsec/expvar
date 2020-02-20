4cde8f2be1647c6781e98095994e545436535e83
Remco Verhoef <remco@dutchcoders.io>
2020-02-20 14:17:28 +0100

    add string and map types

--- expvar.go
+++ expvar.go
@@ -10,6 +10,7 @@ import (
 	"runtime"
 	"sort"
 	"strconv"
+	"strings"
 	"sync"
 	"sync/atomic"
 )
@@ -83,6 +84,153 @@ func (v *Float) Set(value float64) {
 	atomic.StoreUint64(&v.f, math.Float64bits(value))
 }
 
+// Map is a string-to-Var map variable that satisfies the Var interface.
+type Map struct {
+	m      sync.Map // map[string]Var
+	keysMu sync.RWMutex
+	keys   []string // sorted
+}
+
+func (v *Map) String() string {
+	var b strings.Builder
+	fmt.Fprintf(&b, "{")
+	first := true
+	v.Do(func(kv KeyValue) {
+		if !first {
+			fmt.Fprintf(&b, ", ")
+		}
+		fmt.Fprintf(&b, "%q: %v", kv.Key, kv.Value)
+		first = false
+	})
+	fmt.Fprintf(&b, "}")
+	return b.String()
+}
+
+// Init removes all keys from the map.
+func (v *Map) Init() *Map {
+	v.keysMu.Lock()
+	defer v.keysMu.Unlock()
+	v.keys = v.keys[:0]
+	v.m.Range(func(k, _ interface{}) bool {
+		v.m.Delete(k)
+		return true
+	})
+	return v
+}
+
+// addKey updates the sorted list of keys in v.keys.
+func (v *Map) addKey(key string) {
+	v.keysMu.Lock()
+	defer v.keysMu.Unlock()
+	// Using insertion sort to place key into the already-sorted v.keys.
+	if i := sort.SearchStrings(v.keys, key); i >= len(v.keys) {
+		v.keys = append(v.keys, key)
+	} else if v.keys[i] != key {
+		v.keys = append(v.keys, "")
+		copy(v.keys[i+1:], v.keys[i:])
+		v.keys[i] = key
+	}
+}
+
+func (v *Map) Get(key string) Var {
+	i, _ := v.m.Load(key)
+	av, _ := i.(Var)
+	return av
+}
+
+func (v *Map) Set(key string, av Var) {
+	// Before we store the value, check to see whether the key is new. Try a Load
+	// before LoadOrStore: LoadOrStore causes the key interface to escape even on
+	// the Load path.
+	if _, ok := v.m.Load(key); !ok {
+		if _, dup := v.m.LoadOrStore(key, av); !dup {
+			v.addKey(key)
+			return
+		}
+	}
+
+	v.m.Store(key, av)
+}
+
+// Add adds delta to the *Int value stored under the given map key.
+func (v *Map) Add(key string, delta int64) {
+	i, ok := v.m.Load(key)
+	if !ok {
+		var dup bool
+		i, dup = v.m.LoadOrStore(key, new(Int))
+		if !dup {
+			v.addKey(key)
+		}
+	}
+
+	// Add to Int; ignore otherwise.
+	if iv, ok := i.(*Int); ok {
+		iv.Add(delta)
+	}
+}
+
+// AddFloat adds delta to the *Float value stored under the given map key.
+func (v *Map) AddFloat(key string, delta float64) {
+	i, ok := v.m.Load(key)
+	if !ok {
+		var dup bool
+		i, dup = v.m.LoadOrStore(key, new(Float))
+		if !dup {
+			v.addKey(key)
+		}
+	}
+
+	// Add to Float; ignore otherwise.
+	if iv, ok := i.(*Float); ok {
+		iv.Add(delta)
+	}
+}
+
+// Delete deletes the given key from the map.
+func (v *Map) Delete(key string) {
+	v.keysMu.Lock()
+	defer v.keysMu.Unlock()
+	i := sort.SearchStrings(v.keys, key)
+	if i < len(v.keys) && key == v.keys[i] {
+		v.keys = append(v.keys[:i], v.keys[i+1:]...)
+		v.m.Delete(key)
+	}
+}
+
+// Do calls f for each entry in the map.
+// The map is locked during the iteration,
+// but existing entries may be concurrently updated.
+func (v *Map) Do(f func(KeyValue)) {
+	v.keysMu.RLock()
+	defer v.keysMu.RUnlock()
+	for _, k := range v.keys {
+		i, _ := v.m.Load(k)
+		f(KeyValue{k, i.(Var)})
+	}
+}
+
+// String is a string variable, and satisfies the Var interface.
+type String struct {
+	s atomic.Value // string
+}
+
+func (v *String) Value() string {
+	p, _ := v.s.Load().(string)
+	return p
+}
+
+// String implements the Var interface. To get the unquoted string
+// use Value.
+func (v *String) String() string {
+	s := v.Value()
+	b, _ := json.Marshal(s)
+	return string(b)
+}
+
+func (v *String) Set(value string) {
+	v.s.Store(value)
+}
+
 var Default = &Bucket{}
 
 type Bucket struct {
@@ -119,6 +267,34 @@ func Get(name string) Var {
 func (m *Bucket) Get(name string) Var {
 	i, _ := m.vars.Load(name)
 	v, _ := i.(Var)
+	return v
+}
+
+func NewMap(name string) *Map {
+	return Default.NewMap(name)
+}
+
+func (m *Bucket) NewMap(name string) *Map {
+	if v := m.Get(name); v != nil {
+		return v.(*Map)
+	}
+
+	v := new(Map)
+	m.Publish(name, v)
+	return v
+}
+
+func NewString(name string) *String {
+	return Default.NewString(name)
+}
+
+func (m *Bucket) NewString(name string) *String {
+	if v := m.Get(name); v != nil {
+		return v.(*String)
+	}
+
+	v := new(String)
+	m.Publish(name, v)
 	return v
 }
 
